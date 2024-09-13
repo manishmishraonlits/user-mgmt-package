e88dc045e8b8e5e2c041aa0ef72fcb56023a59d8
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2024-09-13 09:19:55 +0530

    first commit

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,39 @@
+namespace Ols2401/UserMgmtPackage/Http/Controllers;
+
+class UserController
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+        
+    }
+}
